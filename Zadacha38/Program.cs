b58bcff95ee00d897c803bd478de81d1a3c1b9ce
Sergey <arakcheev1@yandex.ru>
2022-12-29 21:54:39 +0500

    add zadacha38

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int[] Res(int[] diff)
+{
+    int max = diff[0];
+    int min = diff[0];
+    for (int i = 0; i < diff.Length; i++)
+    {
+        if (diff[i] > max)
+        {
+            max = diff[i];
+        }
+        if (diff[i] < min)
+        {
+            min = diff[i];
+        }
+    }
+    Console.WriteLine($"{max}  {min}");
+    return new int[] { max, min };
+}
